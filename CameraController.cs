0671afadc8c56ca2f39f2a94923736a3ab0b2b9e
Lohann R <83791163+kohann13@users.noreply.github.com>
2024-01-18 23:19:44 -0300

    Update CameraController.cs

--- CameraController.cs
+++ CameraController.cs
@@ -7,27 +7,20 @@ public class CameraController : MonoBehaviour
 {
     private Transform Target;
     [SerializeField]private float SmoothSpeed;
-    private GameObject GC;
 
     // Start is called before the first frame update
     void Start()
     {
         Target = GameObject.FindGameObjectWithTag("Player").transform;
-        GC = GameObject.FindGameObjectWithTag("Clock");
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (GC.GetComponent<GameController>().Colisions() == false)
-        {
-            Vector3 StartPosition = new Vector3(Target.position.x, Target.position.y + 6.12f, -1f);
-            Vector3 SmoothPosition = Vector3.Lerp(transform.position, StartPosition, SmoothSpeed);
-            transform.position = SmoothPosition;
-        }
-        else 
-        {
-            transform.position = new Vector3(0f, 0f, -1f);
-        }
+        Vector3 StartPosition = new Vector3(Target.position.x, Target.position.y, -1f);
+        Vector3 SmoothPosition = Vector3.Lerp(transform.position, StartPosition, SmoothSpeed);
+        transform.position = SmoothPosition;
+            
+        transform.position = new Vector3(0f, 0f, -1f);
     }
 }
